b7859a7d66fdf54ec5ab2c0c9a49d31502b966dd
byungi <zeronin2@gmail.com>
2021-02-22 21:05:33 +0900

    clang-format

--- src/leetcode/1_twosum.c
+++ src/leetcode/1_twosum.c
@@ -1,30 +1,30 @@
 // https://leetcode.com/problems/two-sum/
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "libtap/tap.h"
 
 int *twosum(int *nums, int len, int target, int *ret_len) {
-  int *ret = malloc(2 * sizeof(int));
-  size_t i, j;
-  for (i = 0; i < len; i++) {
-    for (j = i + 1; j < len; j++) {
-      if (nums[j] == target - nums[i])
-        goto EXIT;
+    int *ret = malloc(2 * sizeof(int));
+    size_t i, j;
+    for (i = 0; i < len; i++) {
+        for (j = i + 1; j < len; j++) {
+            if (nums[j] == target - nums[i]) goto EXIT;
+        }
     }
-  }
 EXIT:
-  ret[0] = i;
-  ret[1] = j;
-  *ret_len = 2;
-  return ret;
+    ret[0] = i;
+    ret[1] = j;
+    *ret_len = 2;
+    return ret;
 }
 
 int main(void) {
-  int nums[] = {2, 7, 11, 15};
-  int ret_len;
-  int nums_len = (int)(sizeof(nums) / sizeof(int));
-  int *ret = twosum(nums, nums_len, 9, &ret_len);
-  cmp_mem(ret, (int[]){0, 1}, 2);
-  free(ret);
-  return 0;
+    int nums[] = {2, 7, 11, 15};
+    int ret_len;
+    int nums_len = (int)(sizeof(nums) / sizeof(int));
+    int *ret = twosum(nums, nums_len, 9, &ret_len);
+    cmp_mem(ret, (int[]){0, 1}, 2);
+    free(ret);
+    return 0;
 }
