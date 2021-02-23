00da36a2fcdf9813ad9b4e36626bd2ced6614c1c
byungi <tiniwz@gmail.com>
2021-02-23 19:01:40 +0900

    fix

--- src/programmers/30_12901_2016year.c
+++ src/programmers/30_12901_2016year.c
@@ -4,7 +4,7 @@
 #include "libtap/tap.h"
 
 char* solution(int a, int b) {
-    char* str = (char*)malloc(sizeof(char) * 3);
+    char* str = (char*)malloc(sizeof(char) * 4);
     while (--a) b += a == 2 ? 29 : (int[]){30, 31}[a > 7 ? !(a % 2) : a % 2];
     return str = (char*[]){"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"}[(4 + b) % 7];
 }
