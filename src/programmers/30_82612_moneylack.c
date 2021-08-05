c45f5648dc6c6ccaec51773ba3a804496cea4158
byungi <zeronin2@gmail.com>
2021-08-05 23:14:38 +0900

    프로그래머스 위클리머시기 1주차

---
+++ src/programmers/30_82612_moneylack.c
@@ -0,0 +1,13 @@
+#include "libtap/tap.h"
+#include "math.h"
+#include "stdio.h"
+
+long long solution(int price, int money, int count) {
+    long long n = price * ((pow(count, 2) + count) / 2) - money;
+    return n < 0 ? 0 : n;
+}
+
+int main() {
+    ok(solution(3, 20, 4) == 10);
+    return 0;
+}
